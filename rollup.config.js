0b6419a5dadff51f854de754b941fe60de096b2c
Zack Arnett <arnett.zackary@gmail.com>
2020-08-02 11:53:22 -0500

    fail at minimizing the npm package size, v 0.0.13

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,60 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
+import filesize from "rollup-plugin-filesize";
 
 export default [
   {
     input: "src/lit-draggable.ts",
     output: {
       file: "dist/lit-draggable.js",
-      format: "es",
+      format: "esm",
       sourcemap: "inline",
     },
     plugins: [
       typescript(),
       nodeResolve({
         extensions: [".js", ".ts"],
-        preferBuiltins: false,
-        browser: true,
       }),
-      terser(),
+      terser({
+        warnings: true,
+        ecma: 2017,
+        compress: {
+          unsafe: true,
+        },
+        output: {
+          comments: false,
+        },
+      }),
+      filesize({
+        showBrotliSize: true,
+      }),
     ],
   },
   {
     input: "src/lit-draggable-wrapper.ts",
     output: {
       file: "dist/lit-draggable-wrapper.js",
-      format: "es",
+      format: "esm",
     },
     plugins: [
       typescript(),
       nodeResolve({
         extensions: [".js", ".ts"],
-        preferBuiltins: false,
-        browser: true,
       }),
-      terser(),
+      terser({
+        warnings: true,
+        ecma: 2017,
+        compress: {
+          unsafe: true,
+        },
+        output: {
+          comments: false,
+        },
+      }),
+      filesize({
+        showBrotliSize: true,
+      }),
     ],
   },
 ];
